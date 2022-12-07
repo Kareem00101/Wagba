aac5d45b718e0c6bbc5b5842596f2d2165b282a1
Kareem Ayman <89580954+Kareem00101@users.noreply.github.com>
2022-12-08 00:58:08 +0200

    Tested Code & Added Assets

--- app/src/main/java/com/example/wagba/MainActivity.java
+++ app/src/main/java/com/example/wagba/MainActivity.java
@@ -2,22 +2,10 @@ package com.example.wagba;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.wagba.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
 {
